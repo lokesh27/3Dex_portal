2aca0c58fa31495e656aec06ac82d13f7dfa48af
Lokesh Tuteja <lokesh.tuteja@yahoo.co.in>
2016-03-05 01:17:43 +0530

    Check for specific uploads

--- uploads/forms.py
+++ uploads/forms.py
@@ -1,6 +1,12 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class UploadForm(forms.Form):
-    stlfile = forms.FileField(
-        label='Select a file',
-    )
+    def validate_file_extension(value):
+        import os
+        ext = os.path.splitext(value.name)[1]
+        valid_extensions = ['.stl','.obj']
+        if not ext in valid_extensions:
+            raise ValidationError(u'Please upload valid stl or obj')
+
+    stlfile = forms.FileField(label='Select a file',validators=[validate_file_extension])
